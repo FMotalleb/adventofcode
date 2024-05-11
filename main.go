5c51cb6d2a95f484d629464d44ddb6d025196e3a
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2024-05-11 23:28:28 +0330

    feat: done the quest

--- main.go
+++ main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"log"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
-var (
-	codeMap map[byte]int32 = map[byte]int32{
-		40: 1,
-		41: -1,
-	}
-)
-
-func main() {
+func loadQuestion() []byte {
 	file, err := os.Open("code.txt")
 	if err != nil {
 		log.Panicln("open file with error", err)
@@ -23,20 +19,50 @@ func main() {
 	}
 	buffer := make([]byte, stat.Size())
 	file.Read(buffer)
-	var position int32 = 0
-	reported := false
-	for index, item := range buffer {
-		position += codeMap[item]
-		// Part 2
-		if reported != true && position == -1 {
-			log.Println("Part 2 answer:", index+1)
-			reported = true
-		}
+	file.Close()
+	return buffer
+}
+
+func main() {
+	lines := strings.Split(string(loadQuestion()), "\n")
+	totalArea, totalRibbon := 0, 0
+	for _, item := range lines {
+		g := parseGift(item)
+
+		totalArea += g.calculateArea()
+		totalRibbon += g.calculateRibbon()
+
 	}
-	// Part 1
-	log.Println("Part 1 answer:", position)
+	log.Println("Part 1:", totalArea, "and Part 2:", totalRibbon)
 }
 
 func init() {
 	log.SetOutput(os.Stdout)
+}
+
+type gift struct {
+	dimensions *[3]int
+}
+
+func (g gift) calculateArea() int {
+	dims := g.dimensions
+	return dims[0]*dims[1] + 2*dims[0]*dims[1] + 2*dims[1]*dims[2] + 2*dims[0]*dims[2]
+}
+func (g gift) calculateRibbon() int {
+	dims := g.dimensions
+	return dims[0]*2 + dims[1]*2 + dims[0]*dims[1]*dims[2]
+}
+
+func parseGift(data string) gift {
+	dimensions := strings.Split(data, "x")
+	a, _ := strconv.Atoi(dimensions[0])
+	b, _ := strconv.Atoi(dimensions[1])
+	c, _ := strconv.Atoi(dimensions[2])
+	finalDimensions := &[3]int{
+		a, b, c,
+	}
+	sort.Ints(finalDimensions[:])
+	return gift{
+		finalDimensions,
+	}
 }
