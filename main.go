a7ba86bed18c4e1376c70918212a813171c4e3fe
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2024-05-11 23:44:44 +0330

    reset

--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"log"
 	"os"
-	"sort"
-	"strconv"
-	"strings"
 )
 
 func loadQuestion() []byte {
@@ -24,45 +21,9 @@ func loadQuestion() []byte {
 }
 
 func main() {
-	lines := strings.Split(string(loadQuestion()), "\n")
-	totalArea, totalRibbon := 0, 0
-	for _, item := range lines {
-		g := parseGift(item)
 
-		totalArea += g.calculateArea()
-		totalRibbon += g.calculateRibbon()
-
-	}
-	log.Println("Part 1:", totalArea, "and Part 2:", totalRibbon)
 }
 
 func init() {
 	log.SetOutput(os.Stdout)
-}
-
-type gift struct {
-	dimensions *[3]int
-}
-
-func (g gift) calculateArea() int {
-	dims := g.dimensions
-	return dims[0]*dims[1] + 2*dims[0]*dims[1] + 2*dims[1]*dims[2] + 2*dims[0]*dims[2]
-}
-func (g gift) calculateRibbon() int {
-	dims := g.dimensions
-	return dims[0]*2 + dims[1]*2 + dims[0]*dims[1]*dims[2]
-}
-
-func parseGift(data string) gift {
-	dimensions := strings.Split(data, "x")
-	a, _ := strconv.Atoi(dimensions[0])
-	b, _ := strconv.Atoi(dimensions[1])
-	c, _ := strconv.Atoi(dimensions[2])
-	finalDimensions := &[3]int{
-		a, b, c,
-	}
-	sort.Ints(finalDimensions[:])
-	return gift{
-		finalDimensions,
-	}
 }
