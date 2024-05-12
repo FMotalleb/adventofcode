92306a9a0266b491410eb48ea5d2f3e9761ad8a0
FMotalleb <m.falahnejad@dornica.net>
2024-05-12 12:08:00 +0330

    done part one

--- main.go
+++ main.go
@@ -5,6 +5,15 @@ import (
 	"os"
 )
 
+var (
+	operators map[byte][2]int = map[byte][2]int{
+		94:  {1, 0},
+		62:  {0, 1},
+		118: {-1, 0},
+		60:  {0, -1},
+	}
+)
+
 func loadQuestion() []byte {
 	file, err := os.Open("code.txt")
 	if err != nil {
@@ -21,7 +30,18 @@ func loadQuestion() []byte {
 }
 
 func main() {
-
+	resultTable := map[[2]int]int{
+		{0, 0}: 1,
+	}
+	currentPos := [2]int{0, 0}
+	com := loadQuestion()
+	for _, operation := range com {
+		operator := operators[operation]
+		currentPos[0] += operator[0]
+		currentPos[1] += operator[1]
+		resultTable[currentPos]++
+	}
+	log.Println("all houses", len(resultTable))
 }
 
 func init() {
